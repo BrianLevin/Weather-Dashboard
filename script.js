bfbc39e05fac515608d4ce869a93b403c2b93a59
Brian Levin <bml201095@gmail.com>
2020-04-04 22:32:24 -0400

    moved some html in the JS file to make visually easier to add some javascript. within it I included a Moment.js file file showing the day and time when I append the weather and included a response.list to include the Tempuerate the the humidity

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 
 $(document).ready(function () { // This will start a jquery values
 
-function showIcon(icon) { // This function will run through which Icon to show depending on the temperature.
-    if (icon === "Rain") {
-        return '<i class= fas fa-cloud-rain></i>';
-    } else if(icon===  "Sunny")
-    return  ' <i class=  fas fa-2x icon></i>';
-}
+    function showIcon(icon) { // This function will run through which Icon to show depending on the temperature.
+        if (icon === "Rain") {
+            return '<i class= fas fa-cloud-rain></i>';
+        } else if (icon === "Sunny") {
+            return '<i class=  fas fa-2x icon></i>';
 
+        }
+        console.log(icon);
+    }
 
     $.ajax({
         url: 'https://api.openweathermap.org/data/2.5/forecast?q=Austin,Texas&appid=bcd5953a34ae60d3a19dd457376dfad5&units=imperial' // This ajax call will call the weather api for the city. I use my own gernarted api and I changed the units from kelvin to farenheight.
@@ -15,16 +17,22 @@ function showIcon(icon) { // This function will run through which Icon to show d
     }).then(function (response) {  // This will run the responce once the api is genrrated by appendin the result onto the card.
         for (var i = 0; i < response.list.length; i += 8) {  // the for loop will go through respoce list every 3 hours within a 24 hours period to get the weather.
             $('.forecasts').append(
-                <div class= "card">
-                    <div class= "card-body">
-            <h3 class = "card-title"> ${responce.list[i].dt_text}</h3>
-            
-            $[showIcon(response.weather[0].main)]
+
+                `<div class="col-xs-12 col-sm-12 col-md-3 col-lg-4 col-xl-4">  <!-- This html will be apended on to the html page-->
+                    <div class= "card">
+                        <div class= "card-body">
+                            <h3 class = "card-title"> ${moment(response.list[i].dt_txt).format('MM/DD/YYYY')}</h3> <!-- This Moment.js will get the month, day and time onto the page -->
+                            ${showIcon(response.list[i].weather[0].main)} <!--  This will display the weather from the for loop -->
+                            
+                            <p> temperature: ${response.list[i].main.temp} </p>  <!-- This will display the temperature from the looped day. -->
+                            <p>  humidity: ${response.list[i].main.humidity} </p>  <!-- This will display the humidity from the looped day -->
             
                         
                         </div> 
+                    </div> 
                 </div>
 
+                `
             );
             console.log(response.list[i]);
         }
@@ -32,7 +40,7 @@ function showIcon(icon) { // This function will run through which Icon to show d
 
 
 
-}
+
 
 
 
