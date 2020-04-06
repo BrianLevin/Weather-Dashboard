76467ec770033639c6387fdcb9b00a6210581553
Brian Levin <bml201095@gmail.com>
2020-04-05 20:41:08 -0400

    added read me text

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Weather-Dashboard
+
+# Weather Dashboard
+
+
+
+## Assignment objectives:
+
+The purpose of this assignment was  to create a weather dashboard which projected the 5 day forecast from a selected city. This included the temperature, humidity, and weather for each day.
+
+## Syntax Used
+For this assignment, I used html and css to display the weather dashboard syntax and styling onto the website. I then created a API for the weather.org and made an Ajax  call to fetch the 5 day forecasts for the city. I used pure Javascript for the if else statements and Jquery to append the api data onto the page. I also used a Moment.js to append the day month and year. I used Jquery to append the temperature and the humidity 
+
+
+
+## What I learned
+
+I learned how to get an API Key and then use an ajax call to append  the api key for to get weather.. I then learned additional Jquery to append the specific weather response onto the page.
--- script.js
+++ script.js
@@ -7,6 +7,10 @@ $(document).ready(function () { // This will start a jquery values
         } else if (icon === "Sunny") {
             return '<i class=  fas fa-2x icon></i>';
 
+        } else if (icon === "Cloudy") {
+            return '<i class= fas fa-cloud</i>';
+        } else if (icon === "Snow") {
+            return '<i class= fas fa-snowflake</i>';
         }
         console.log(icon);
     }
@@ -35,7 +39,9 @@ $(document).ready(function () { // This will start a jquery values
                 `
             );
             console.log(response.list[i]);
+
         }
+
     })
 
 
@@ -53,7 +59,7 @@ $(document).ready(function () { // This will start a jquery values
     /*    var xhttp = new XMLHttpRequest();
     var apiKey = "bcd5953a34ae60d3a19dd457376dfad5";
     var city = 'Austin';
-
+ 
     // wait for the responce from the server
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.state == 200) {   //  this shows the data did not come back with any eoors.
@@ -61,14 +67,14 @@ $(document).ready(function () { // This will start a jquery values
             console.log(data);
             //temperatures
         }
-
+ 
     };
-
+ 
     // sends the Git Request to the server
     xhttp.opem("GET", `https://api.openweathermap.org/data/2.5/forecast?q=Austin,Texas&appid=bcd5953a34ae60d3a19dd457376dfad5`, true);
     xhttp.send();
 });
-
-
+ 
+ 
 */
 });
