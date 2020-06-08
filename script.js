c5bdb2be0c258350944e16988630248e26ae0661
Brian Levin <bml201095@gmail.com>
2020-06-08 15:30:41 -0400

    created a method which will cleae the search value

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
     $("#search-button").on("click", function () {
         var searchValue = $("#search-value").val();
+        // clear input box
+        $("#search-value").val("");
 
-
+        searchWeather(searchValue);
     });
+
+});
