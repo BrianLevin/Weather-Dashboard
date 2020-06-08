3670b87b04c6608490446b8275e9284d78eca2ba
Brian Levin <bml201095@gmail.com>
2020-06-08 15:44:40 -0400

    created a function which will hold my ajax call with my api key to get the weather for each city and then push  and save the seach value in local storage

--- script.js
+++ script.js
@@ -14,4 +14,19 @@ $(document).ready(function () {
         var li = $("<li>").addClass("list-group-item list-group-item-action").text(text);
         $(".history").append(li);
     }
-});
+    function searchWeather(searchValue) {
+        $.ajax({
+            type: "GET",
+            url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=bcd5953a34ae60d3a19dd457376dfad5&units=imperial",
+            dataType: "json",
+            success: function (data) {
+                // create history link for this search
+                if (history.indexOf(searchValue) === -1) {
+                    history.push(searchValue);
+                    window.localStorage.setItem("history", JSON.stringify(history));
+
+                    makeRow(searchValue);
+                }
+
+            });
+
